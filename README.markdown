fd313acd7751ed71ed04549d86fc161dd3db0a0b
totte <totte@tott.es>
2013-09-04 19:48:01 +0200

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# hashcomp
+hashcomp compares the hashes of two files.
+
+## Usage
+hashcomp.py <file1> <file2>
---
+++ hashcomp.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import sys
+import hashlib
+
+def hashfile(filename, hasher, blocksize=65536):
+    filebuffer = filename.read(blocksize)
+    while len(filebuffer) > 0:
+        hasher.update(filebuffer)
+        filebuffer = filename.read(blocksize)
+    return hasher.hexdigest()
+
+try:
+    with open(sys.argv[1], 'rb') as f:
+        file1 = hashfile(f, hashlib.sha512())
+
+    with open(sys.argv[2], 'rb') as f:
+        file2 = hashfile(f, hashlib.sha512())
+
+    if file1 == file2:
+        print(sys.argv[1] + ", " + sys.argv[2] + ": MATCH")
+    else:
+        print(sys.argv[1] + ", " + sys.argv[2] + ": MISS")
+except IndexError:
+    print("Usage: " + sys.argv[0] + " <file1> <file2>")
+except IOError:
+    print("File not found: ")
+
